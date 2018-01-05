4cade39ab6ec2f47d7bafa27e252bdc209c2deb0
Adriano Interaminense <adriano.interaminense@liferay.com>
2018-01-04 23:13:47 -0300

    initial commit

---
+++ __tests__/Math.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import Math from '../src/Math';
+
+describe('Math', function() {
+	it('should be tested', function() {
+		expect('No tests for this module yet.').toBe('Everything is ok.');
+	});
+});
---
+++ src/Math.js
@@ -0,0 +1,12 @@
+import JSXComponent from 'metal-jsx';
+
+import './math.scss';
+
+class Math extends JSXComponent {
+	render() {
+		return <div class="math">Hello World</div>;
+	}
+}
+
+export { Math };
+export default Math;
