ebb462059891e9a5d7491542282f0700fe01d8d6
Adriano Interaminense <adriano.interaminense@liferay.com>
2018-01-12 01:10:34 -0300

    Update component Layout

---
+++ src/components/Layout.js
@@ -0,0 +1,28 @@
+import Component, {Config} from 'metal-jsx';
+import {CLASSNAME} from './../Utils';
+
+const Header = (props) => {
+	return <div class={`${CLASSNAME}__layout-header`}>{props.children}</div>
+}
+const Body = (props) => {
+	return <div class={`${CLASSNAME}__layout-body`}>{props.children}</div>
+}
+const Footer = (props) => {
+	return <div class={`${CLASSNAME}__layout-footer`}>{props.children}</div>
+}
+
+class Layout extends Component {
+	render() {
+		return(
+			<div class={`${CLASSNAME}__layout`}>
+				{this.props.children}
+			</div>
+		);
+	}
+}
+
+Layout.Header = Header;
+Layout.Body = Body;
+Layout.Footer = Footer;
+
+export default Layout;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 import Button from './Button';
+import Layout from './Layout';
 
-export { Button };
+export { Button, Layout };
