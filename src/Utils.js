69becfae9516aee667129a04d5c14866b8ffbf9d
Adriano Interaminense <adriano.interaminense@liferay.com>
2018-01-12 01:10:49 -0300

    Adds Language

--- src/Utils.js
+++ src/Utils.js
@@ -1,3 +1,17 @@
 const CLASSNAME = 'mathematics';
 
-export {CLASSNAME};
+const LANGUAGE = {
+	errors: 'errors',
+	hits: 'hits',
+	next: 'next',
+	ops: 'Ops, try again!',
+	totalErrors: 'total erros',
+	totalHits: 'total hits',
+	yeah: 'Yeah, congratulations!',
+	initGame: 'Init game',
+	startGame: 'Start game',
+	finishGame: 'Finish game',
+	startGameAgain: 'Start game again'
+}
+
+export {CLASSNAME, LANGUAGE};
