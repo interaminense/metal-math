4f999c0c9505cf1ea71ebed06a60dc57ba6cb20d
Adriano Interaminense <adriano.interaminense@liferay.com>
2018-01-12 01:09:55 -0300

    Update component Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,31 @@ import Component, {Config} from 'metal-jsx';
 import {CLASSNAME} from './../Utils';
 
 class Button extends Component {
+	classActive() {
+		return this.props.isActive ? `${CLASSNAME}-btn--active` : '';
+	}
+
 	render() {
 		return (
 			<button
 				{...this.otherProps()}
-				class={`${CLASSNAME}-btn ${CLASSNAME}-btn--default`}>
+				class={`
+					${CLASSNAME}-btn
+					${this.classActive()}
+					${CLASSNAME}-btn--${this.props.className}
+				`}>
 				{this.props.children}
 			</button>
 		);
 	}
 }
+
+Button.PROPS = {
+	isActive: Config.bool().value(false),
+	className: Config.oneOf([
+		'primary',
+		'default'
+	]).value('default')
+};
 
 export default Button;
