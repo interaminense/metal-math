ed30a833db70c637c5cd3f3b9715ef813bba8528
Adriano Interaminense <adriano.interaminense@liferay.com>
2018-01-12 01:11:34 -0300

    Removing comment

--- src/Mathematics.js
+++ src/Mathematics.js
@@ -196,28 +196,6 @@ class Mathematics extends Component {
 							type="number"
 							required
 						/>
-
-						{/* <div>
-							<button type="button" name="7">7</button>
-							<button type="button" name="8">8</button>
-							<button type="button" name="9">9</button>
-						</div>
-						<div>
-							<button type="button" name="4">4</button>
-							<button type="button" name="5">5</button>
-							<button type="button" name="6">6</button>
-						</div>
-						<div>
-							<button type="button" name="1">1</button>
-							<button type="button" name="2">2</button>
-							<button type="button" name="3">3</button>
-						</div>
-						<div>
-							<button type="button" name="0">0</button>
-						</div>
-
-						<button type="submit">{LANGUAGE.next}</button> */}
-
 						<div>
 							<Button type="button" name="7">7</Button>
 							<Button type="button" name="8">8</Button>
