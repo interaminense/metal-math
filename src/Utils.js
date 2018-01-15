e25c9a77ffca22b7965075beaf4632716acfa03e
Adriano Interaminense <adriano.interaminense@liferay.com>
2018-01-15 09:55:52 -0300

    Adds method Calculate

--- src/Utils.js
+++ src/Utils.js
@@ -4,14 +4,29 @@ const LANGUAGE = {
 	errors: 'errors',
 	hits: 'hits',
 	next: 'next',
-	ops: 'Ops, try again!',
+	ops: '🤦 Ops, try again!',
 	totalErrors: 'total erros',
 	totalHits: 'total hits',
-	yeah: 'Yeah, congratulations!',
-	initGame: 'Init game',
-	startGame: 'Start game',
-	finishGame: 'Finish game',
-	startGameAgain: 'Start game again'
-}
+	yeah: '👏 Yeah, congratulations!',
+	initGame: '🎮 Mathematics',
+	startGame: '🏁 Start game',
+	finishGame: '🎯 Game Over',
+	startGameAgain: '🏁 Start game again',
+	time: '🕑'
+};
 
-export {CLASSNAME, LANGUAGE};
+const CALCULATE = (n1, n2, operator) => {
+	let result = 0;
+
+	switch(operator) {
+		case '+': result = n1 + n2; break;
+		case '-': result = n1 - n2; break;
+		case 'x': result = n1 * n2; break;
+		case '÷': result = n1 / n2; break;
+		default: result = null; break;
+	}
+
+	return result.toFixed(0);
+};
+
+export {CLASSNAME, LANGUAGE, CALCULATE};
