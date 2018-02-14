784b11606c70a4ebb5f15fdae9afe3742352de6c
Adriano Interaminense <adriano.interaminense@liferay.com>
2018-02-14 00:30:19 -0300

    Passing api values to state

--- src/App.js
+++ src/App.js
@@ -3,45 +3,57 @@ import Mathematics from './components/Mathematics';
 
 class App extends Component {
 	render() {
+		const {lvlDefault, lvls, showResult, time} = this.state;
+
 		return (
-			<Mathematics
-				lvlDefault={'normal'}
-				lvls={[
-					{
-						internalLabel: 'easy',
-						label: '😸 easy',
-						maxNumber: 3,
-						operators: [
-							{ label: '+', },
-							{ label: '-', }
-						]
-					},
-					{
-						internalLabel: 'normal',
-						label: '🧠 normal',
-						maxNumber: 5,
-						operators: [
-							{ label: '+', },
-							{ label: '-', },
-							{ label: 'x', }
-						]
-					},
-					{
-						internalLabel: 'hard',
-						label: '💀 hard',
-						maxNumber: 10,
-						operators: [
-							{ label: '+', },
-							{ label: '-', },
-							{ label: 'x', },
-							{ label: '÷', }
-						]
-					}
-				]}
-				showResult={false}
-				time={30}
-			/>
+			<Mathematics lvlDefault={lvlDefault} lvls={lvls} showResult={showResult} time={time} />
 		);
+	}
+}
+
+App.STATE = {
+	lvlDefault: {
+		value: 'easy'
+	},
+	lvls: {
+		value: [
+			{
+				internalLabel: 'easy',
+				label: '😸 easy',
+				maxNumber: 3,
+				operators: [
+					{ label: '+', },
+					{ label: '-', }
+				]
+			},
+			{
+				internalLabel: 'normal',
+				label: '🧠 normal',
+				maxNumber: 5,
+				operators: [
+					{ label: '+', },
+					{ label: '-', },
+					{ label: 'x', }
+				]
+			},
+			{
+				internalLabel: 'hard',
+				label: '💀 hard',
+				maxNumber: 10,
+				operators: [
+					{ label: '+', },
+					{ label: '-', },
+					{ label: 'x', },
+					{ label: '÷', }
+				]
+			}
+		]
+	},
+	showResult: {
+		value: false
+	},
+	time: {
+		value: 20
 	}
 }
 
