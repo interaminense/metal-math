b404df45497d531f5e6be2f706e4f3a2f1316942
Adriano Interaminense <adriano.interaminense@liferay.com>
2018-02-27 23:13:51 -0300

    Update utils

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,9 +1,4 @@
 /**
- * Class name default
- */
-const CLASSNAME = 'mathematics';
-
-/**
  * Object list language
  */
 const LANGUAGE = {
@@ -59,4 +54,4 @@ const getRandomNumber = (min, max) => {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export {CLASSNAME, LANGUAGE, CALCULATE, getCalcTimeoutAmout, getRandomNumber};
+export {LANGUAGE, CALCULATE, getCalcTimeoutAmout, getRandomNumber};
